204744c58e0d1295a0f89d4c8d1c308905f1d0ed
Andre Perez <andre.marcos.perez@gmail.com>
2019-02-25 01:18:24 -0300

    updated nav bar mostly

--- static/javascript/navbar.js
+++ static/javascript/navbar.js
@@ -1,12 +1,10 @@
 $('p').click(function() {
-  if ($(this).hasClass('side-content')) {
+  if ($(this).hasClass('nav-content')) {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
         });
     });
-    $(".side-content-highlighted").removeClass("side-content-highlighted").addClass("side-content");
-    $(this).removeClass("side-content").addClass("side-content-highlighted");
     document.getElementById($(this).parent().attr("href").substring(1)).scrollIntoView({behavior: "smooth"});
   }
 });
